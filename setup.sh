b37c478bebfdb3666e0815fa5e568d1754b5eab1
kubocker <kubocker@gmail.com>
2017-09-09 13:49:29 +0900

    brew cask

--- setup.sh
+++ setup.sh
@@ -47,7 +47,8 @@ if [ $(uname -s) = 'Darwin' ]; then
 
     # Homebrew
     [ -z "$(which brew)" ] &&
-        ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+        ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" < /dev/null 2> /dev/null ; brew install caskroom/cask/brew-cask 2> /dev/null
+        
 
     echo "Brewfile....."
     brew tap Homebrew/bundle
