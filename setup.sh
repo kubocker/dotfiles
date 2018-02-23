2f45f6f31fd64c79f67856e4cd4927438f572e78
kubocker <kubocker@gmail.com>
2018-02-23 14:22:41 +0900

    comment delete

--- setup.sh
+++ setup.sh
@@ -1,22 +1,5 @@
 #!/bin/bash
 
-# color list
-##################################################################
-# Black        0;30     Dark Gray     1;30
-# Red          0;31     Light Red     1;31
-# Green        0;32     Light Green   1;32
-# Brown/Orange 0;33     Yellow        1;33
-# Blue         0;34     Light Blue    1;34
-# Purple       0;35     Light Purple  1;35
-# Cyan         0;36     Light Cyan    1;36
-# Light Gray   0;37     White         1;37
-
-# ex.
-#RED='\033[0;31m'
-#NC='\033[0m' # No Color
-#printf "I ${RED}love${NC} Stack Overflow\n"]']'
-#################################################################
-
 # setting
 kbk_dir=Develop/kubocker/dotfiles
 
@@ -142,10 +125,6 @@ do
   # ln -s $dir/dotfiles/$file $HOME/$file
   ln -s $dir/dotfiles/dotfiles/$file $HOME/$file
 done
-
-# kbk vims
-# git clone git@github.com:kubocker/userautoload.git $HOME/Develop/kubocker/userautoload
-# ln -nfs $HOME/Develop/kubocker/userautoload $HOME/.vim/userautoload
 
 # vim-plug
 echo "Installing Vim Plugin"
