603de7c635ffa40579d653615c93f2bd76d2efa3
kubocker <kubocker@gmail.com>
2018-02-26 16:55:03 +0900

    add pyenv and setup pip

--- apps/pip/_install.sh
+++ apps/pip/_install.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+echo "pyenv install...."
+git clone https://github.com/yyuu/pyenv.git ~/.pyenv
+
+echo "install python 3.6.0...."
+pyenv versions
+pyenv install 3.6.0
+pyenv global 3.6.0
+pyenv rehash
+
 which -s pip
 if [[ $? != 0 ]] ; then
     echo "install pip !!"
