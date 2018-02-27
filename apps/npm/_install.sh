54e6020e4b84833c76311db4c038e192fe782d71
kubocker <kubocker@gmail.com>
2018-02-27 10:15:18 +0900

    npm packages add and fix nvm to nodenv

--- apps/npm/_install.sh
+++ apps/npm/_install.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
 
-echo "install nvm..."
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.26.1/install.sh | bash
-
-which -s nvm
-if [[ $? != 0 ]] ; then
-  echo "install nvm with first.... please"
-  return
-fi
-
 echo "install global npm packages..."
 cat Npmfile | xargs npm install -g
