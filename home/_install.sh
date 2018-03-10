05f5f396bd20d16d846d68049c1f85a159eeea26
kubocker <kubocker@gmail.com>
2018-03-10 21:19:32 +0900

    iterm colorschmes add

--- home/_install.sh
+++ home/_install.sh
@@ -3,3 +3,4 @@
 mkdir ~/Develop/kubocker/dotfiles/home/terminal
 # iterm colors
 git clone https://github.com/dracula/iterm.git ~/Develop/kubocker/dotfiles/home/terminal/dracula
+git clone git@github.com:mbadolato/iTerm2-Color-Schemes.git ~/Develop/kubocker/dotfiles/home/terminal/mbadolato
