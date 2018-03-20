f003159d330569e1fbf01b8057c94da9a0384952
kubocker <kubocker@gmail.com>
2018-03-20 19:30:37 +0900

    fix terminal install

--- home/_install.sh
+++ home/_install.sh
@@ -1,6 +1,9 @@
 #! /bin/bash
 
-mkdir ~/Develop/kubocker/dotfiles/home/terminal
+
+DIR = $HOME/Develop/kubocker/dotfiles
+
 # iterm colors
-git clone https://github.com/dracula/iterm.git ~/Develop/kubocker/dotfiles/home/terminal/dracula
-git clone git@github.com:mbadolato/iTerm2-Color-Schemes.git ~/Develop/kubocker/dotfiles/home/terminal/mbadolato
+mkdir -p $DIR/home/terminal
+git clone https://github.com/dracula/iterm.git $DIR/home/terminal/dracula
+# git clone git@github.com:mbadolato/iTerm2-Color-Schemes.git ~/Develop/kubocker/dotfiles/home/terminal/mbadolato
