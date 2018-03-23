6f14371336e634cde50aa76d50a3e158f93f9f85
kubocker <kubocker@gmail.com>
2018-03-23 15:32:23 +0900

    delete home.installer

--- home/_install.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-
-DIR = $HOME/Develop/kubocker/dotfiles
-
-# iterm colors
-mkdir -p $DIR/home/terminal
-git clone https://github.com/dracula/iterm.git $DIR/home/terminal/dracula
-# git clone git@github.com:mbadolato/iTerm2-Color-Schemes.git ~/Develop/kubocker/dotfiles/home/terminal/mbadolato
