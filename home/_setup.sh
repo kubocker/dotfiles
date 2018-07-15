ef5abffb471cc3efaae82fd3b0eaa10c41da55e1
kubocker <kubocker@gmail.com>
2018-07-15 15:04:31 +0900

    add

--- home/_setup.sh
+++ home/_setup.sh
@@ -6,5 +6,5 @@ DIR = $HOME/Develop/kubocker/dotfiles
 # iterm colors
 mkdir -p $DIR/home/terminal
 git clone https://github.com/dracula/iterm.git $DIR/home/terminal/dracula
-git clone git@github.com:arcticicestudio/nord-iterm2.git
+git clone git@github.com:arcticicestudio/nord-iterm2.git $DIR/home/terminal/nord
 # git clone git@github.com:mbadolato/iTerm2-Color-Schemes.git ~/Develop/kubocker/dotfiles/home/terminal/mbadolato
