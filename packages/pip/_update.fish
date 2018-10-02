f7ffed09a3fbda0c75decef8ecae741a884b3ba5
kubocker <kubocker@gmail.com>
2018-10-02 10:25:52 +0900

    fix packages

--- packages/pip/_update.fish
+++ packages/pip/_update.fish
@@ -1,27 +1,6 @@
-#!/bin/bash
+#!/bin/fish
 
-which -s git
-if [[ $? != 0 ]]; then
-    return
-fi
-
-# 1.
-echo "pyenv install...."
-
-git clone https://github.com/yyuu/pyenv.git ~/.pyenv
-
-# 2.
-echo "install python 3.6.0...."
-
-pyenv versions
-pyenv install 3.6.0
-pyenv global 3.6.0
-pyenv rehash
-
-# 3.
 pip install --upgrade pip
 
-# 4.
 echo "install global pip packages..."
-
 cat Pipfile | xargs pip install
