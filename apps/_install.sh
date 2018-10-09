fa0e7f8889a0061219850a80eb626eeabcbea34f
kubocker <kubocker@gmail.com>
2018-10-09 18:49:16 +0900

    no more depending on original os system manager

--- apps/_install.sh
+++ apps/_install.sh
@@ -9,3 +9,9 @@ function sub_install {
 sub_install homebrew/
 sub_install apt_get/
 sub_install yum/
+
+mkdir $HOME/Develop/kubocker/kbk
+mkdir $HOME/Develop/kubocker/study
+mkdir $HOME/Develop/kubocker/asset
+
+mkdir $HOME/Develop/projects
